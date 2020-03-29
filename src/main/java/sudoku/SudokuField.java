e92d683769aeba1ce13fd8e9aeffc9117faefd5f
224406 Pietrzak Witold <224406@edu.p.lodz.pl>
2020-03-30 00:12:23 +0200

    Created classes SudokuField, SudokuBox, SudokuColumn and SudokuRow, added methods getFieldValue and setFieldValuue to SudokuField class

---
+++ src/main/java/sudoku/SudokuField.java
@@ -0,0 +1,13 @@
+package sudoku;
+
+public class SudokuField {
+    private int value;
+
+    public final int getFieldValue() {
+        return value;
+    }
+
+    public final void setFieldValue(int value) {
+        this.value = value;
+    }
+}
